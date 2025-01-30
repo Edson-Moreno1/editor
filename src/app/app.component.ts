8e7d31994c726189e64526fb91534ec527455457
Edson-Moreno1 <edson.morenoortega@gmail.com>
2025-01-29 21:27:45 -0600

    Proyecto editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'editor';
-}
+  title = 'editortexto';
+
+  isTextareaEnabled: boolean = false;
+  blogText: string = "";
+  footerDate: number = new Date().getFullYear();
+
+  //Valores seleccionados
+  selectedFont: string = "Arial";
+  selectedSize: string = "12";
+  selectedColor: string = "black";
+
+  //Habilitar textarea
+  enableTextarea(): void {
+    this.isTextareaEnabled = true;
+  }
+}// CIERRE DE LA CLASE AppComponent
