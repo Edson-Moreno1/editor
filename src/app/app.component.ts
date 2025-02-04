e5806f982b887582f60f3ddfb7ec80f6ab2f0b02
Edson-Moreno1 <edson.morenoortega@gmail.com>
2025-02-03 22:36:49 -0600

    Nueva Actualizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -19,9 +20,17 @@ export class AppComponent {
   selectedFont: string = "Arial";
   selectedSize: string = "12";
   selectedColor: string = "black";
+  selectedLineHeight: string = "1.5";
 
   //Habilitar textarea
   enableTextarea(): void {
     this.isTextareaEnabled = true;
   }
+
+  //Limpiar textarea
+
+  clearTextarea(): void {
+    this.blogText = "";
+  }
+
 }// CIERRE DE LA CLASE AppComponent
